5399a7ab65703e87f89c303c58b361d08b57dc95
Avjay <abhar@student.unimelb.edu.au>
2025-04-19 16:39:54 +1000

    Add activeNote, updateNote functionality

--- 02 - Frontend/final/src/App.jsx
+++ 02 - Frontend/final/src/App.jsx
@@ -6,37 +6,37 @@ import './App.css';
 
 function App() {
   
-  //Create state array to save notes
-  const [notes, setNotes] = useState([]);
+    //Create state array to save notes
+    const [notes, setNotes] = useState([]);
 
-  // State to store the currently edited note. Default value is `false`.
-  const [activeNote, setActiveNote] = useState(false);
+    // State to store the currently edited note. Default value is `false`.
+    const [activeNote, setActiveNote] = useState(false);
 
-  // This is a function to add a new note, defined using arrow notation.
-  // It is defined in the main `App` component as it relies on the `notes` state.
-  const onAddNote = () => {
+    // This is a function to add a new note, defined using arrow notation.
+    // All of the below functions are defined in the main `App` component as they 
+    // rely on the `notes` state.
+    const onAddNote = () => {
 
     console.log("Added new note");
 
     //Everything below defines the default information of a newly created note
     const newNote = {
-      id: uuid(), //This is the `key` value of a list item. It must be unique.
+        id: uuid(), //This is the `key` value of a list item. It must be unique to identify the item in the array.
 
-      title: "Untitled Note",
+        title: "Untitled Note",
 
-      body: "",
-      
-      lastModified: Date.now()
+        body: " ",
+        
+        lastModified: Date.now()
     };
 
     //Set the `notes` state to be a new array containing `newNote`, plus all the 
     //elements of the previous `notes` state. Note the usage of the `spread` operator.
     setNotes([newNote, ...notes])
-  }
+    }
 
-  // This is a function to delete a specified note, defined using conventional notation.
-  // It is defined in the main `App` component as it relies on the `notes` state.
-  function onDeleteNote(idToDelete) {
+    // This function deletes a specified note, and is defined using conventional notation.
+    function onDeleteNote(idToDelete) {
 
     console.log("Removed note");
 
@@ -45,19 +45,44 @@ function App() {
     // into the new `notes` state. 
     setNotes(notes.filter((note) => note.id !== idToDelete))
 
-  }
-  
-  return (
-  <>
-  <title>CISSA NotesApp 2025 :)</title>
-  
-  <div className='App'>
+    }
 
-  <Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote}/>
-  <MainContent/>
-  </div>
-  </>
-  );
+    const onUpdateNote = (updatedNote) => {
+        // Create an entirely new array to replace the `notes` state, by looping through each element of `notes`, and 
+        // replacing the currently active note with the new (updated) note passed into the function. 
+        //
+        // All non-active notes are just copied over to the new array, before it is set as the new `notes` state.
+        const updatedNotesArray = notes.map((note) => {
+            if (note.id === activeNote) {
+                return updatedNote
+            } else {
+                return note;
+            }
+        });
+
+        // Make the new notes array the `notes` array
+        setNotes(updatedNotesArray);
+    }
+
+    // This function returns the note object that is currently listed as `active` in the `activeNote` state.
+    const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote);
+    }
+
+    return (
+    <>
+    <title>CISSA NotesApp 2025 :)</title>
+
+    <div className='App'>
+
+    <Sidebar notes={notes} onAddNote={onAddNote} onDeleteNote={onDeleteNote} activeNote={activeNote} setActiveNote={setActiveNote}/>
+    
+    {/*NOTE that here we WANT the `getActiveNote` function to be called (and it's result passed into MainContent) every time it renders. 
+    Therefore, we do NOT need to append an empty function call `() =>` to the beginning.*/}
+    <MainContent activeNote={getActiveNote()} onUpdateNote={onUpdateNote}/>
+    </div>
+    </>
+    );
 
 }
 
--- 02 - Frontend/final/src/MainContent.jsx
+++ 02 - Frontend/final/src/MainContent.jsx
@@ -1,14 +1,42 @@
-function MainContent() {
+function MainContent({activeNote, onUpdateNote}) {
+
+    function onEditField(textFieldKey, editedValue) {
+        // We need to pass through a NEW note, with the new info, EXCEPT id.
+        onUpdateNote({
+            // This spreads out the elements of the active note.
+            ...activeNote,
+            
+            [textFieldKey]: editedValue,
+            lastModified: Date.now()
+        });
+    };
+
+    // We NEED to accound for the case where there is no current activeNote, otherwise 
+    // the rendering will fail below when we try to access the activeNote attributes.
+    if(!activeNote) {
+        return ( 
+            <div className="no-active-note"> 
+                <h6>No note selected. Create or select one to get started.</h6>
+            </div>
+        )}
+
     return (
         <div className="app-main">
             <div className = "app-main-note-edit">
-                <input type="text" id="title" placeholder="Title here..." autoFocus />
-                <textarea id="body" placeholder="Write your note here..."/>
+                <input type="text" id="title" placeholder="Title here..." value = {activeNote.title} autoFocus />
+                <textarea id="body" placeholder="Write your note here..." value = {activeNote.body}/>
             </div>
+
+            {/* Whatever the current activeNote is, preview it's stored information*/}
             <div className="app-main-note-preview">
                 
-                <h1 className="preview-title">TITLE</h1>
-                <div className="markdown-preview">note preview</div>
+                <h1 className="preview-title"> 
+                    {activeNote.title}
+                </h1>
+
+                <div className="markdown-preview">
+                    {activeNote.body}
+                </div>
 
             </div>    
         </div>
